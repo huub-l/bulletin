52766b2d2f45cce96e46591279778ce10792b2fa
Xiaojun Deng <xdeng@apn-gcr.org>
2017-11-27 17:40:55 +0900

    Remove fade.

--- resources/views/partials/home-page-about.blade.php
+++ resources/views/partials/home-page-about.blade.php
@@ -9,9 +9,9 @@
     </div>
     <div class="col-lg-3 offset-lg-2">
       @if(has_post_thumbnail($page))
-        <img class="sb-fade" src="{{get_the_post_thumbnail_url($page, 'medium')}}" alt="{{get_the_title()}}" width="100%" />
+        <img src="{{get_the_post_thumbnail_url($page, 'medium')}}" alt="{{get_the_title()}}" width="100%" />
       @else
-        <img class="sb-fade"  src="//via.placeholder.com/248x248?text=Science+Bulletin" alt="{{get_the_title()}}">
+        <img src="//via.placeholder.com/248x248?text=Science+Bulletin" alt="{{get_the_title()}}">
       @endif
     </div>
   </div>
