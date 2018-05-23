2377b21ad3ba1fbf373783620048c98bd974ab93
X <hiukuan.tang@gmail.com>
2018-05-23 07:15:31 +0000

    Apply fixes from StyleCI

--- app/setup/article/meta.php
+++ app/setup/article/meta.php
@@ -7,26 +7,23 @@ namespace App;
  * https://github.com/APN-ComDev/bulletin/issues/66
  */
 
-add_action( 'wp_head', function () {
+add_action('wp_head', function () {
+    if ('article' == get_post_type()) {
+        $authors = App::sbGetCoauthorsByPostId(get_the_ID());
 
-    if ( 'article' == get_post_type() ) {
+        // Article title
+        echo '<meta name="citation_title" content="'.get_the_title().'">'."\n";
 
-       $authors = App::sbGetCoauthorsByPostId(get_the_ID());
+        // Article authors
+        foreach ($authors as $author) {
+            echo '<meta name="citation_author" content="'
+               .$author->last_name.', '.$author->first_name.'">'."\n";
+        }
 
-       // Article title
-       echo '<meta name="citation_title" content="'. get_the_title() .'">' ."\n";
+        // Publication date
+        echo '<meta name="citation_publication_date" content="'.get_the_date('Y/m/d').'">'."\n";
 
-       // Article authors
-       foreach($authors as $author) {
-         echo '<meta name="citation_author" content="'
-               . $author->last_name . ', ' . $author->first_name .'">' ."\n";
-       }
-
-       // Publication date
-       echo '<meta name="citation_publication_date" content="'. get_the_date('Y/m/d') .'">' ."\n";
-
-       // Journal title
-       echo '<meta name="citation_journal_title" content="APN Science Bulletin">' ."\n";
-   }
-
+        // Journal title
+        echo '<meta name="citation_journal_title" content="APN Science Bulletin">'."\n";
+    }
 });
