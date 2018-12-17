266c8bfacc86fc781ccae12cc8532301e15d69a0
Xiaojun Deng <xdeng@apn-gcr.org>
2018-12-17 14:53:23 +0900

    Rename files.

---
+++ app/setup/post-type-article.php
@@ -0,0 +1,35 @@
+<?php
+
+add_action('init', function () {
+
+    /**
+     * Post Type: SB Articles.
+     */
+    $labels = [
+        'name'          => __('SB Articles', 'sage'),
+        'singular_name' => __('SB Article', 'sage'),
+    ];
+
+    $args = [
+        'label'               => __('SB Articles', 'sage'),
+        'labels'              => $labels,
+        'description'         => 'Articles in a science bulletin',
+        'public'              => true,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'show_in_rest'        => false,
+        'rest_base'           => '',
+        'has_archive'         => true,
+        'show_in_menu'        => true,
+        'exclude_from_search' => false,
+        'capability_type'     => 'post',
+        'map_meta_cap'        => true,
+        'hierarchical'        => false,
+        'rewrite'             => ['slug' => 'article', 'with_front' => false],
+        'query_var'           => true,
+        'supports'            => ['title', 'editor', 'thumbnail', 'excerpt', 'custom-fields', 'comments', 'author'],
+        'taxonomies'          => ['keyword', 'programme', 'issue', 'partners', 'category'],
+    ];
+
+    register_post_type('article', $args);
+});
