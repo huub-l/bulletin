8ebdf0510cafc253c868b27e5758ab078a807332
Xiaojun Deng <xdeng@apn-gcr.org>
2020-02-04 16:28:51 +0900

    Close hktang/bulletin#107

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,10 +3,21 @@
     <a class="brand" href="{{ home_url('/') }}">
       <img src="{{get_template_directory_uri()}}/images/sb-logo.png" width="237" height="36">
     </a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
+    <div class="d-xs-block d-md-none">
+      <i id="top-nav-burger" class="fa fa-bars nav-sm"></i>
+      <i id="top-nav-quit" class="fa fa-times nav-sm hidden"></i>
+    </div>
+
+    @if (has_nav_menu('primary_navigation'))
+      <nav class="nav-primary d-none d-md-block">
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      </nav>
+    @endif
+
+    <div class="nav-sm hidden d-xs-block d-md-none">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['menu_id' => 'top-nav-links']) !!}
       @endif
-    </nav>
+    </div>
   </div>
 </header>
